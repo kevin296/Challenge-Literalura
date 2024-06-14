922892206d6b23b3e7650bb1469eca5b117bc2af
Kevin Cruz <devs-dgrv@ip.gob.hn>
2024-06-14 13:36:14 -0600

    Inicio

---
+++ src/main/java/com/alura/Challenge_Literalura/ChallengeLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.alura.Challenge_Literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraluraApplication.class, args);
+	}
+
+}
